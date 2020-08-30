4e4e7c97bb71360daa1ff24f3b15dc3ca1b32f18
Lena <lenach241@gmail.com>
2020-08-29 21:31:45 -0500

    feat: add logic

--- src/components/PageContent/AnswerOptions/AnswerOptions.tsx
+++ src/components/PageContent/AnswerOptions/AnswerOptions.tsx
@@ -1,17 +1,40 @@
-import React from "react";
-import styles from './AnswerOption.module.css';
+import React, {useEffect, useState} from "react";
+import styles from './AnswerOptions.module.css';
+import BirdsData from './../../../assets/data/BirdsData';
+import AnswerOption from "./AnswerOption/AnswerOption";
 
-const answerOptions = ['Разминка', 'Воробьиные', 'Лесные птицы', 'Певчие птицы', 'Хищные птицы', 'Морские птицы'];
-const AnswerOptions: React.FC = () => {
-  return <ul className={`text-color ${styles.AnswerOptionList}`}>
-      {answerOptions.map((item, key)=>
-        <li className={styles.AnswerOption} key={key}>
-          <span className={styles.ListItemMarker}/>{item}
-        </li>
-      )}
-    </ul>
+const AnswerOptions: React.FC<{currentCategory: number, currentQuestion: any, updateScore: (attemptCount: number)=>void  }> =
+  ({currentCategory, currentQuestion, updateScore}) => {
+    const [answeredOptions, setAnsweredOptions] = useState<number[]>([]);
+    useEffect(()=>{
+      setAnsweredOptions([])
+    }, [currentQuestion])
+    let attemptCount = 0;
+    const checkAnswer = (id: number) => {
+      if (answeredOptions.includes(id) || answeredOptions.includes(currentQuestion.id)) {
+        return;
+      }
+      setAnsweredOptions(answeredOptions.concat(id).slice());
+      if (id !== currentQuestion.id) {
+        attemptCount += 1;
+      } else {
+        updateScore(attemptCount);
+      }
+    }
+    const getListItemColor = (id: number) => {
+      if (answeredOptions.includes(id) ) {
+        if (id === currentQuestion.id) {
+          return styles.ListItemMarkerCorrect;
+        }
+        return styles.ListItemMarkerIncorrect;
+      }
+      return '';
+    }
 
-
+    return <ul className={`text-color ${styles.AnswerOptionList}`} >
+      {BirdsData[currentCategory].map((item, key)=>
+        <AnswerOption item={item} key={key} listItemColor={getListItemColor(item.id)} checkAnswer={checkAnswer}/>)}
+           </ul>
 }
 
 
--- src/components/PageContent/NextLevelButton/NextLevelButton.tsx
+++ src/components/PageContent/NextLevelButton/NextLevelButton.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from './NextLevelButton.module.css'
 
-const NextLevelButton: React.FC = () => {
-  return <button className={styles.NextLevelButton}>Next Level</button>
+const NextLevelButton: React.FC<{nextButtonActive: boolean, goToNextQuestion: ()=>void }> = ({nextButtonActive, goToNextQuestion}) => {
+  const toNextQuestionHandler = () => goToNextQuestion();
+  return <button className={styles.NextLevelButton + ' ' + (nextButtonActive ?  styles.NextLevelButtonActive : '')}
+                 onClick={nextButtonActive? toNextQuestionHandler : ()=>{}}>Next Level</button>
 }
 
 export default NextLevelButton;
