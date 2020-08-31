dd7f22c1ca96d34127b9299ae2882632bd5e2c54
Lena <lenach241@gmail.com>
2020-08-31 13:00:49 -0500

    fix: fix logic and styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Logo from "./Logo/Logo";
-import GameCount from "./GameScore/GameScore";
 import Categories from "./Categories/Categories";
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+import GameScore from "./GameScore/GameScore";
 
 const Header: React.FC<{currentCategory: number, gameScore: number}> = ({currentCategory, gameScore}) => {
     return <header>
             <div className={styles.TopPanel}>
               <Logo/>
-              <GameCount gameScore={gameScore}/>
+              <GameScore gameScore={gameScore}/>
             </div>
             <Categories currentCategory={currentCategory}/>
           </header>;
--- src/components/PageContent/PageContent.tsx
+++ src/components/PageContent/PageContent.tsx
@@ -6,35 +6,59 @@ import NextLevelButton from "./NextLevelButton/NextLevelButton";
 import BirdsData from "../../assets/data/BirdsData";
 import Header from "../Header/Header";
 import CategoryList from "../../assets/const/CategoryList";
+import GameOver from "./GameOver/GameOver";
 
 const PageContent: React.FC = () => {
+
   const getRandomQuestion = () => BirdsData[currentCategory][ Math.floor(Math.random() * BirdsData[currentCategory].length)];
+
   const [currentCategory, setCategory] = useState<number>(CategoryList[0].id);
   const [gameScore, setScore] = useState<number>(0);
   const [nextButtonActive, setNextButtonToActive] = useState<boolean>(false);
   const [currentQuestion, setCurrentQuestion] = useState(getRandomQuestion());
+  const [gameFinished, setGameToFinish] = useState<boolean>(false);
+
   const updateScore: (attemptCount: number) => void = (attemptCount) => {
     setNextButtonToActive(true);
     setScore(gameScore+(5-attemptCount));
   }
+
   const goToNextQuestion = () => {
-    setCategory(currentCategory+1);
-    setCurrentQuestion(getRandomQuestion());
-    setNextButtonToActive(false);
+    if (currentCategory < BirdsData.length-1) {
+      setCategory(currentCategory+1);
+      setCurrentQuestion(getRandomQuestion());
+      setNextButtonToActive(false);
+    } else {
+      setGameToFinish(true);
+    }
   }
+
+  const restartGame = () => {
+    setCategory(CategoryList[0].id);
+    setCurrentQuestion(getRandomQuestion());
+    setScore(0);
+    setGameToFinish(false);
+  }
+
+ if (gameFinished) {
     return <>
-              <Header currentCategory={currentCategory} gameScore={gameScore}/>
-              <CurrentQuestion currentQuestion={currentQuestion} />
-              <div className='row'>
-                <div className='col col-6'>
-                  <AnswerOptions currentCategory={currentCategory} currentQuestion={currentQuestion} updateScore={updateScore} />
-                </div>
-                <div className='col col-6'>
-                  <BirdInfo nextButtonActive={nextButtonActive} currentQuestion={currentQuestion} />
-                </div>
+      <Header currentCategory={currentCategory} gameScore={gameScore}/>
+      <GameOver restartGame={restartGame} finalScore={gameScore} />
+    </>
+ }
+return <>
+            <Header currentCategory={currentCategory} gameScore={gameScore}/>
+            <CurrentQuestion currentQuestion={currentQuestion} />
+            <div className='row'>
+              <div className='col col-6'>
+                <AnswerOptions currentCategory={currentCategory} currentQuestion={currentQuestion} updateScore={updateScore} />
               </div>
-              <NextLevelButton nextButtonActive={nextButtonActive} goToNextQuestion={goToNextQuestion}/>
-            </>
+              <div className='col col-6'>
+                <BirdInfo nextButtonActive={nextButtonActive} currentQuestion={currentQuestion} />
+              </div>
+            </div>
+            <NextLevelButton nextButtonActive={nextButtonActive} goToNextQuestion={goToNextQuestion}/>
+          </>
 
 }
 
